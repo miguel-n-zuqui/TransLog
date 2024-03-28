55ed9a523ea0d6a334d9889983af472bc67af774
Senai <Senai@CEPEAS21104012>
2024-03-27 21:32:51 -0300

    Feat: Terminado programa da translog obs: possiveis atualizacoes podem ser feita

---
+++ TransLog/FronEnd/Model/Request/FreteRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FronEnd.Model.Request
+{
+    public class FreteRequest
+    {
+        [Required(ErrorMessage = "Este campo é obrigatoro é obrigatório")]
+        public decimal Distancia { get; set; }
+        [Required(ErrorMessage = "Este campo é obrigatoro é obrigatório")]
+        public decimal Altura { get; set; }
+        [Required(ErrorMessage = "Este campo é obrigatoro é obrigatório")]
+        public decimal Largura { get; set; }
+        [Required(ErrorMessage = "Este campo é obrigatoro é obrigatório")]
+        public decimal PesoCarga { get; set; }
+    }
+}
